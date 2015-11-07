11fcc08ca86f5f1c2c9cd340bc549531cbd04f31
Dane Smith <danesmith@Tiffany-Renandos-MacBook-Air.local>
2015-11-07 15:27:49 -0600

    fix all admin bug

--- server/routes/index.js
+++ server/routes/index.js
@@ -67,7 +67,7 @@ router.route('/submit')
 
 
 router.get('/*', function(req, res){
-    var file = req.params[0] || "/views/admin.html";
+    var file = req.params[0] || "/views/index.html";
     res.sendFile(path.join(__dirname, "../public", file))
 });
 
